55f53f49cb0dbf6b5cf5983116938df500cc9e7b
piyushmishra7709 <141897336+piyushmishra7709@users.noreply.github.com>
2023-08-11 16:00:07 +0530

    positive or negative

---
+++ positive_or_negative.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main() 
+{
+    int n;
+    printf("Enter any number:");
+    scanf("%d", &n);
+    if(n>0){
+        printf("Number is positive");
+    }
+    else{
+        printf("Number is negative");
+    }
+}
