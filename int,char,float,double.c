08a92384f2ea8ae8c7a37afe48e092b94bed50be
piyushmishra7709 <141897336+piyushmishra7709@users.noreply.github.com>
2023-08-11 16:28:50 +0530

    int,char,float,double

---
+++ int,char,float,double.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main() 
+{
+  int a=10;
+  char c='a';
+  float f=2.33;
+  double d=34.4567;
+  printf("%d \n", a);
+  printf("%c \n", c);
+  printf("%f \n", f);
+  printf("%fl \n", d); 
+}
