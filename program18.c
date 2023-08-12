d0370d20f0df2d7efc7995f236aa714db6658037
piyushmishra7709 <141897336+piyushmishra7709@users.noreply.github.com>
2023-08-12 17:06:08 +0530

    print int, char, float

---
+++ program18.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main() 
+{
+  int a=10;
+  char c='a';
+  float f=2.33;
+
+  printf("Integer is %d \n", a);
+  printf("Charcter is %c \n", c);
+  printf("Float is %f \n", f);
+}
