022e8007def0d6015f4aa0da5908bf12c3cfa3b5
piyushmishra7709 <141897336+piyushmishra7709@users.noreply.github.com>
2023-08-20 11:58:05 +0530

    odd no. 1 to n

--- program15.c
+++ program15.c
@@ -5,10 +5,9 @@ int main()
     printf("Enter any number: ");
     scanf("%d",&n);
     
-    for (int i=1; i<n-1; i++){
-        int res = i + 2;
-        if (res % 2 != 0){
-            printf("Odd number:%d \n",res);
+    for (int i=1; i<=n; i++){
+        if (i % 2 != 0){
+            printf("Odd number:%d \n",i);
         }
     }
     return 0;
