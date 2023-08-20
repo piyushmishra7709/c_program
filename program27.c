4513723d4c945a2043bd47c833c06f302edd43b4
piyushmishra7709 <141897336+piyushmishra7709@users.noreply.github.com>
2023-08-20 13:33:36 +0530

    square of a no.

---
+++ program27.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() 
+{
+    int a;
+    printf("Enter any number :");
+    scanf("%d",&a);
+    
+    int sqr=a*a;
+    printf("Square of %d is :%d",a,sqr);
+    return 0;
+}
