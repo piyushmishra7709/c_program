5b6be8073f352ce9ea213944768ba06bdfd98287
piyushmishra7709 <141897336+piyushmishra7709@users.noreply.github.com>
2023-08-20 17:05:10 +0530

    multiply 2 floating point no.

---
+++ program40.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    float a,b;
+    
+    printf("Enter first number :");
+    scanf("%f",&a);
+    printf("Enter second number :");
+    scanf("%f",&b);
+    
+    float c=a*b;
+    printf("Multiplication is :%f",c);
+    return 0;
+}
